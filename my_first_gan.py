6b456c3552f18a9931ee784b1aaf147b9dbb528d
jan.szczekulski <jan.szczekulski@thehut.local>
2021-01-30 15:51:48 +0000

    Add another part to first gan

--- my_first_gan.py
+++ my_first_gan.py
@@ -16,10 +16,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IPython.display import HTML
+from parameters import *
 
 # Set random seed for reproducibility
 manualSeed = 999
 #manualSeed = random.randint(1, 10000) # use if you want new results
-print("Random Seed: ", manualSeed)
+print("loop - should only be shwown once")
 random.seed(manualSeed)
 torch.manual_seed(manualSeed)
+
+
+
+
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+dataset = dset.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+print("batch loaded")
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+print("almost show it ")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+print("It did finish loop")
